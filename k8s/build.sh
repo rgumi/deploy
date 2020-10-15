60e45d18a9a5f7b89f953b64a445c4f7d3879f0f
Ron Gummich <ron.gummich@telekom.de>
2020-10-15 14:54:10 +0200

    error in gitlab version

--- k8s/build.sh
+++ k8s/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+set -ex
+echo "Deploying version $DEPOY_TAG"
 
 NS=ingress-depoy
 kubectl create ns $NS |true
 
 kustomize edit set image depoy=erpk/depoy:$DEPOY_TAG
-kustomize build | tee build.yaml
 
+kustomize build | tee build.yaml
 kubectl apply -f build.yaml
 rm -f build.yaml
